5df7046efd001e2a6b410309897cccec454b6546
aritra-123-ux <mr.aritramaity07@gmail.com>
2026-06-06 19:34:58 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+ let count=0;
+ function increase(){
+    count++;
+    document.getElementById("count").innerText=count;
+ }
+function decrease(){
+    count--;
+    document.getElementById("count").innerText=count;
+}
+function changeText(){
+    document.getElementById("title").innerText="Hello Aritra";
+}
